94f4c7ce27d176c45eb26f1c86c71e67bd99cf72
samijihd <khwairehsami@gmail.com>
2020-05-01 23:41:05 +0300

    login and signup -sami

---
+++ app/src/main/java/com/example/askdoctors/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.askdoctors.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.askdoctors.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/askdoctors/Activities/LoginActivity.java
+++ app/src/main/java/com/example/askdoctors/Activities/LoginActivity.java
@@ -1,16 +1,47 @@
 package com.example.askdoctors.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.askdoctors.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText Email = findViewById(R.id.email_input);
+        EditText Password = findViewById(R.id.password_input);
+    }
+
+
+    private void LoginUser(String email, String password) {
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "login successfully", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
